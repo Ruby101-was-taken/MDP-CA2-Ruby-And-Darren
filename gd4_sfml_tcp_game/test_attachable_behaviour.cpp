d2123672c46aa05841dccc07d8315abb62129fa2
Darren <D00255479@student.dkit.ie>
2026-02-19 11:42:16 +0000

    ♻️ Update files names and ordering

--- gd4_sfml_tcp_game/test_attachable_behaviour.cpp
+++
@@ -1,7 +0,0 @@
-#include "test_attachable_behaviour.hpp"
-#include <iostream>
-#include "scene_node.hpp"
-
-void TestAttachableBehaviour::Update(sf::Time dt, CommandQueue& commands) {
-	std::cout << node_->name_ << std::endl;
-}
