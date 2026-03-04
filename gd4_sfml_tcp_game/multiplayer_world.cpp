b2dcc7002a708803c109be22b093bd1db66b6ec1
Ruby <abusinessemailkinda@gmail.com>
2026-03-04 20:08:34 +0000

    🚧  Add game server to multiplayer world

--- gd4_sfml_tcp_game/multiplayer_world.cpp
+++ gd4_sfml_tcp_game/multiplayer_world.cpp
@@ -1,9 +1,64 @@
 #include "multiplayer_world.hpp"
+#include <SFML/Network/Packet.hpp>
+#include <SFML/Network/IpAddress.hpp>
+#include <fstream>
+#include "network_protocol.hpp"
 
-MultiplayerWorld::MultiplayerWorld(sf::RenderTarget& output_target, FontHolder& font, SoundPlayer& sounds, State::Context* context) : GameWorld(output_target, font, sounds, context) {
+sf::IpAddress GetAddressFromFile() {
+	{
+		//Try to open existing file
+		std::ifstream input_file("ip.txt");
+		std::string ip_address;
+		if (input_file >> ip_address) {
+			if (auto address = sf::IpAddress::resolve(ip_address)) {
+				return *address;
+			}
+		}
+	}
+
+	//If the open/read failed, create a new file
+	std::ofstream output_file("ip.txt");
+	sf::IpAddress local_address = sf::IpAddress::LocalHost;
+	output_file << local_address.toString();
+	return local_address;
+}
+
+MultiplayerWorld::MultiplayerWorld(sf::RenderTarget& output_target, FontHolder& font, SoundPlayer& sounds, State::Context* context) : 
+	GameWorld(output_target, font, sounds, context) ,
+	game_server_(nullptr),
+	is_host_(true),
+	is_connected_(false)
+{
 	StartBuildScene();
 }
 
 void MultiplayerWorld::BuildScene() {
 	MakeBaseScene();
+
+	//If this is the host, create a server
+	std::optional<sf::IpAddress> ip;
+	if (is_host_) {
+		game_server_.reset(new GameServer(sf::Vector2f(context_->window->getSize())));
+		ip = sf::IpAddress::LocalHost;
+	}
+	else {
+		ip = GetAddressFromFile();
+	}
+
+	if (ip) {
+		auto status = socket_.connect(*ip, SERVER_PORT, sf::seconds(5.f));
+
+		if (status == sf::Socket::Status::Done) {
+			is_connected_ = true;
+		}
+		else {
+			failed_connection_clock_.restart();
+		}
+	}
+	else {
+		failed_connection_clock_.restart();
+	}
+
+	//Set socket to non-blocking
+	socket_.setBlocking(false);
 }
--- gd4_sfml_tcp_game/multiplayer_world.hpp
+++ gd4_sfml_tcp_game/multiplayer_world.hpp
@@ -1,10 +1,19 @@
 #pragma once
 #include "game_world.hpp"
+#include "game_server.hpp"
 
 class MultiplayerWorld : public GameWorld {
 public:
 	MultiplayerWorld(sf::RenderTarget& output_target, FontHolder& font, SoundPlayer& sounds, State::Context* context);
 
 	void BuildScene() override;
+
+private:
+	bool is_host_;
+	bool is_connected_;
+
+	std::unique_ptr<GameServer> game_server_;
+	sf::TcpSocket socket_;
+	sf::Clock failed_connection_clock_;
 };
 
