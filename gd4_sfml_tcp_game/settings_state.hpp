19364ab9b6b19007682207d363b810795757ab6f
Darren <D00255479@student.dkit.ie>
2026-03-04 15:02:38 +0000

    🐛 Fix settings and pause compiler errors

---
+++ gd4_sfml_tcp_game/settings_state.hpp
@@ -0,0 +1,140 @@
+// Darren - D00255479
+// Ruby White - D00255322
+#pragma once
+#include "state.hpp"
+#include "menu_options.hpp"
+#include "container.hpp"
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <vector>
+
+template <typename WorldClass>
+class SettingsState : public State {
+public:
+    SettingsState(StateStack& stack, Context context);
+    virtual void Draw() override;
+    virtual bool Update(sf::Time dt) override;
+    virtual bool HandleEvent(const sf::Event& event) override;
+
+private:
+    sf::Sprite background_sprite_;
+    gui::Container gui_container_;
+
+    sf::Text sfx_volume_;
+    sf::Text music_volume_;
+};
+
+#include "menu_state.hpp"
+#include "resource_holder.hpp"
+#include "utility.hpp"
+#include "button.hpp"
+
+template <typename WorldClass>
+SettingsState<WorldClass>::SettingsState(StateStack& stack, Context context)
+    :State(stack, context)
+    , background_sprite_(context.textures->Get(TextureID::kTitleScreen))
+    , sfx_volume_(context.fonts->Get(Font::kMain))
+    , music_volume_(context.fonts->Get(Font::kMain)) {
+    //sf::Texture& texture = context.textures->Get(TextureID::kTitleScreen);
+
+    //background_sprite_.setTexture(texture);
+
+    // Darren - D00255479
+    auto back_button = std::make_shared<gui::Button>(context);
+    back_button->setPosition({ 100, 500 });
+    back_button->SetText("Back");
+    back_button->SetCallback([this]() {
+        RequestStackPop();
+        RequestStackPush(StateID::kMenu);
+        });
+
+
+    // Sound Volume
+    // Darren - D00255479
+    auto subtract_sound_volume_button = std::make_shared<gui::Button>(context);
+    subtract_sound_volume_button->SetSmall(true);
+    subtract_sound_volume_button->setPosition({ 500, 500 });
+    subtract_sound_volume_button->SetText("-");
+    subtract_sound_volume_button->SetCallback([this, context]() {
+        context.sounds->IncrementVolume(-20.f);
+        sfx_volume_.setString("SFX: " + std::to_string((int)context.sounds->GetVolume()));
+        });
+
+    // Ruby White - D00255322
+    sfx_volume_.setPosition({ 450, 450 });
+    sfx_volume_.setString("SFX: 100");
+    sfx_volume_.setFillColor(sf::Color::Black);
+
+    // Darren - D00255479
+    auto add_sound_volume_button = std::make_shared<gui::Button>(context);
+    add_sound_volume_button->SetSmall(true);
+    add_sound_volume_button->setPosition({ 700, 500 });
+    add_sound_volume_button->SetText("+");
+    add_sound_volume_button->SetCallback([this, context]() {
+        context.sounds->IncrementVolume(20.f);
+        sfx_volume_.setString("SFX: " + std::to_string((int)context.sounds->GetVolume() / 1));
+        });
+
+    // Music
+    // Ruby White - D00255322
+    auto subtract_music_volume_button = std::make_shared<gui::Button>(context);
+    subtract_music_volume_button->SetSmall(true);
+    subtract_music_volume_button->setPosition({ 700, 500 });
+    subtract_music_volume_button->SetText("-");
+    subtract_music_volume_button->SetCallback([this, context]() {
+        context.music->IncrementVolume(-20.f);
+        music_volume_.setString("Music: " + std::to_string((int)context.music->GetVolume()));
+        });
+
+    // Ruby White - D00255322
+    music_volume_.setPosition({ 800, 450 });
+    music_volume_.setString("Music: 100");
+    music_volume_.setFillColor(sf::Color::Black);
+
+    // Ruby White - D00255322
+    auto add_music_volume_button = std::make_shared<gui::Button>(context);
+    add_music_volume_button->SetSmall(true);
+    add_music_volume_button->setPosition({ 900, 500 });
+    add_music_volume_button->SetText("+");
+    add_music_volume_button->SetCallback([this, context]() {
+        context.music->IncrementVolume(20.f);
+        music_volume_.setString("Music: " + std::to_string((int)context.music->GetVolume() / 1));
+        });
+
+    gui_container_.Pack(back_button);
+    gui_container_.Pack(subtract_sound_volume_button);
+    gui_container_.Pack(add_sound_volume_button);
+    gui_container_.Pack(add_music_volume_button);
+    gui_container_.Pack(subtract_music_volume_button);
+
+    // TODO: Add a unique theme for this menu and settings
+    context.music->Play(MusicThemes::kMenuTheme);
+}
+
+// Darren - D00255479
+template <typename WorldClass>
+void SettingsState<WorldClass>::Draw() {
+    sf::RenderWindow& window = *GetContext().window;
+    window.setView(window.getDefaultView());
+
+    sf::RectangleShape backgroundShape;
+    backgroundShape.setFillColor(sf::Color(0, 0, 0, 150));
+    backgroundShape.setSize(window.getView().getSize());
+
+    window.draw(background_sprite_);
+    window.draw(backgroundShape);
+    window.draw(sfx_volume_);
+    window.draw(music_volume_);
+    window.draw(gui_container_);
+}
+
+template <typename WorldClass>
+bool SettingsState<WorldClass>::Update(sf::Time dt) {
+    return true;
+}
+
+template <typename WorldClass>
+bool SettingsState<WorldClass>::HandleEvent(const sf::Event& event) {
+    gui_container_.HandleEvent(event);
+    return true;
+}
