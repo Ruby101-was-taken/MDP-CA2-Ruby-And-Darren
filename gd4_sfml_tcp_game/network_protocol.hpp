b292d2a803d95991a1d413f8ff462181f4d61700
Darren <D00255479@student.dkit.ie>
2026-04-05 15:29:04 +0100

    🎨 Add player input events to network_protocol

--- gd4_sfml_tcp_game/network_protocol.hpp
+++ gd4_sfml_tcp_game/network_protocol.hpp
@@ -7,7 +7,11 @@ namespace Server {
 		kIAmHost, // tells server which player is the host
 		kAddPlayer, // used when the non host clients need to spawn a new player
 		kStartGame, // used when host presses start
-		kSpawnStar // used when a star spawns. Sends x and y with it
+		kSpawnStar, // used when a star spawns. Sends x and y with it
+
+		// Player Input: networked input packets that clients send to server and that server forwards to host
+		kPlayerEvent,           // username + uint8(action)
+		kPlayerRealtimeChange   // username + uint8(action) + uint8(started)
 	};
 }
 
