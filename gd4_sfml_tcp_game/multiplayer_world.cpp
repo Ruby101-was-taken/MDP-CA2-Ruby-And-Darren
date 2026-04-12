9941bad461526bb3f599a12019822afbe9c459bc
Ruby <abusinessemailkinda@gmail.com>
2026-04-12 21:39:16 +0100

    🐛 Fix wrong music playing bug

--- gd4_sfml_tcp_game/multiplayer_world.cpp
+++ gd4_sfml_tcp_game/multiplayer_world.cpp
@@ -180,7 +180,6 @@ void MultiplayerWorld::HandlePacketType(Server::PacketType type, sf::Packet& dat
 		HandlePlayerLeave(data);
 		break;
 	case Server::PacketType::kStartGame:
-		PlayLevelMusic();
 		if (!is_host_) {
 			HandleGameStart(data);
 		}
@@ -298,6 +297,8 @@ void MultiplayerWorld::StartGame() {
 		sf::Packet start_packet = Utility::CreatePacket(Server::PacketType::kStartGame);
 		start_packet << static_cast<uint8_t>(level_id_);
 		SendPacket(start_packet);
+
+		PlayLevelMusic();
 	}
 }
 
@@ -351,6 +352,8 @@ void MultiplayerWorld::IWon() {
 void MultiplayerWorld::HandleGameStart(sf::Packet& data) {
 	uint8_t level_id;
 	data >> level_id;
+
+	PlayLevelMusic();
 
 	level_id_ = static_cast<int>(level_id);
 
